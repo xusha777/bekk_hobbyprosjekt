1f0869bd39581b21c18e91852f46a9887e41cc4a
xenia_grib <xeniag@stud.ntnu.no>
2026-04-28 17:39:26 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Velkommen! 👋</h1>
+      <p>
+        Du kan se koden for dette prosjektet{" "}
+        <a href="https://github.com/bekk/prosjektutgangspunkt">
+          i repoet på GitHub
+        </a>
+      </p>
+      <p>
+        Du kan endre koden i <code>src/App.jsx</code>
+      </p>
+    </div>
+  );
+}
+
+export default App;
